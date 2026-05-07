c0a1ab418d67ec1561a9e7b8cac1109637a0ae44
tomoki-miso <107928941+tomoki-miso@users.noreply.github.com>
2026-05-07 16:16:59 +0900

    chore : 不要な差分を削除

--- cafe.rb
+++ cafe.rb
@@ -1,32 +1,33 @@
 # frozen_string_literal: true
+
 DRINKS = [
-  { name: "コーヒー", price: "300" },
-  { name: "カフェラテ", price: "400" },
-  { name: "チャイ", price: "460" },
-  { name: "エスプレッソ", price: "340" },
-  { name: "緑茶", price: "450" },
+  { name: 'コーヒー', price: '300' },
+  { name: 'カフェラテ', price: '400' },
+  { name: 'チャイ', price: '460' },
+  { name: 'エスプレッソ', price: '340' },
+  { name: '緑茶', price: '450' }
 ].freeze
 
 FOODS = [
-  { name: "チーズケーキ", price: "470" },
-  { name: "アップルパイ", price: "520" },
-  { name: "ホットサンド", price: "410" },
+  { name: 'チーズケーキ', price: '470' },
+  { name: 'アップルパイ', price: '520' },
+  { name: 'ホットサンド', price: '410' }
 ].freeze
 
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
-  print ">"
+  print '>'
   order_number = gets.to_i - 1
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
 
-puts "bugカフェへようこそ！ご注文は？ 番号でどうぞ"
+puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
 order1 = take_order(DRINKS)
 
-puts "フードメニューはいかがですか?"
+puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
 total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
